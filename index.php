c3bbc471c9894eec98a4abc584414516c31984e1
caefer <caefer@ical.ly>
2011-04-20 11:00:37 +0200

    Switched from using stdClass objects to array and got rid of a whole bunch of notices and strict warnings

--- index.php
+++ index.php
@@ -42,7 +42,7 @@ function render($module) {
 <html lang="en-US" xml:lang="en-US" xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
-    <title><?php echo (isset($data->title) ? $data->title : 'statuspanic generic status board') ?></title>
+    <title><?php echo (isset($data['title']) ? $data['title'] : 'statuspanic generic status board') ?></title>
     <link rel='stylesheet' type='text/css' href='resources/panic.css' />
     <script type='text/javascript' src='resources/jquery.js'></script>
     <script type='text/javascript' src='resources/board.js'></script>
